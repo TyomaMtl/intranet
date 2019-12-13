f26121da8d00139e6031472f9fd13f6620019218
TyomaMtl <scollioudfree.fr@gmail.com>
2019-12-13 18:00:54 +0100

    feat : secure register controller if user already logged

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -9,14 +9,20 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Security;
 
 class RegistrationController extends AbstractController
 {
     /**
      * @Route("/register", name="register")
      */
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    public function register(Security $security, Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
     {
+        if(!is_null($security->getUser()))
+        {
+            return $this->redirectToRoute('home');
+        }
+
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
