bc77f3e787f69b2fc16f0e59790a6c1eb53777bb
TyomaMtl <scollioudfree.fr@gmail.com>
2019-12-19 13:01:57 +0100

    feat : profile user subjects

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,18 +2,25 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Security\Core\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfileController extends AbstractController
 {
+    private $user;
+
+    public function __construct(Security $security)
+    {
+        $this->user = $security->getUser();
+    }
     /**
      * @Route("/profile", name="profile")
      */
     public function index()
     {
         return $this->render('profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
+            'user' => $this->user
         ]);
     }
 }
