d147fddae2e2278c46a67569585227b07e7f7afc
TyomaMtl <scollioudfree.fr@gmail.com>
2019-12-19 13:36:40 +0100

    feat : subscribe / unsubscribe to subject

--- src/Controller/SubjectsController.php
+++ src/Controller/SubjectsController.php
@@ -5,14 +5,21 @@ namespace App\Controller;
 use App\Repository\SubjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class SubjectsController extends AbstractController
 {
+    private $user;
     private $sujects;
+    private $manager;
 
-    public function __construct(SubjectRepository $sujects)
+    public function __construct(Security $security, SubjectRepository $sujects, EntityManagerInterface $manager)
     {
+        $this->user = $security->getUser();
         $this->subjects = $sujects;
+        $this->manager = $manager;
     }
 
     /**
@@ -23,5 +30,43 @@ class SubjectsController extends AbstractController
         return $this->render('subjects/index.html.twig', [
             'subjects' => $this->subjects->findAll()
         ]);
+    }
+
+    /**
+     * @Route("/subject/subscribe/{subjectId}", name="subject_subscribe")
+     * 
+     * Only students can access to this route
+     * @IsGranted("ROLE_STUDENT")
+     */
+    public function subscribe(int $subjectId)
+    {
+        $subject = $this->subjects->findById($subjectId);
+        $subject = $subject[0];
+        
+        $subject->addUser($this->user);
+
+        $this->manager->persist($subject);
+        $this->manager->flush();
+
+        return $this->redirectToRoute('profile');
+    }
+
+    /**
+     * @Route("/subject/unsubscribe/{subjectId}", name="subject_unsubscribe")
+     * 
+     * Only students can access to this route
+     * @IsGranted("ROLE_STUDENT")
+     */
+    public function unsubscribe(int $subjectId)
+    {
+        $subject = $this->subjects->findById($subjectId);
+        $subject = $subject[0];
+        
+        $subject->removeUser($this->user);
+
+        $this->manager->persist($subject);
+        $this->manager->flush();
+
+        return $this->redirectToRoute('profile');
     }
 }
