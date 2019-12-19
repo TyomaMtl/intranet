817181bab0bc12ea700c47c3643ea929ff800b2d
TyomaMtl <scollioudfree.fr@gmail.com>
2019-12-19 12:12:28 +0100

    feat : subject list

--- src/Controller/SubjectsController.php
+++ src/Controller/SubjectsController.php
@@ -2,18 +2,26 @@
 
 namespace App\Controller;
 
+use App\Repository\SubjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SubjectsController extends AbstractController
 {
+    private $sujects;
+
+    public function __construct(SubjectRepository $sujects)
+    {
+        $this->subjects = $sujects;
+    }
+
     /**
      * @Route("/subjects", name="subjects")
      */
     public function index()
     {
         return $this->render('subjects/index.html.twig', [
-            'controller_name' => 'SubjectsController',
+            'subjects' => $this->subjects->findAll()
         ]);
     }
 }
