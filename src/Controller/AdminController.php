52f3856610508c2f43a5863b366879a015a62a5f
TyomaMtl <scollioudfree.fr@gmail.com>
2020-01-01 12:52:11 +0100

    fix : secure admin controller

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,7 +5,11 @@ namespace App\Controller;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
+/**
+ * @IsGranted("ROLE_ADMIN")
+ */
 class AdminController extends AbstractController
 {
     /**
